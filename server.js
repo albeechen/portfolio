d1411cea50f0dc1ef7bea81bc217e09715a33d34
albeechen <linda3632000@hotmail.com>
2017-08-30 16:37:29 -0700

    rewrite server.js

--- server.js
+++ server.js
@@ -1,14 +1,6 @@
-var express = require('express');
-var app = express();
-
-//
-app.use(express.static(__dirname + '/public'));
-app.get('/', (req, res) => {
-	console.log("ENTER GET");
-	res.status = 200;
-	res.send({message: 'test'});
+var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
+var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
+ 
+server.listen(server_port, server_ip_address, function () {
+  console.log( "Listening on " + server_ip_address + ", port " + server_port )
 });
-
-app.listen('8888', () => {
-	console.log("server start");
-})
