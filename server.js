8878f16616e58be81acd6014e64bdc94cf6739b7
albeechen <linda3632000@hotmail.com>
2017-08-30 22:29:21 -0700

    modified code

--- server.js
+++ server.js
@@ -3,6 +3,13 @@ var server = express();
 var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
 var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
  
+server.use(express.static(__dirname + '/public'));
+server.get('/', (req, res) => {
+	console.log("ENTER GET");
+	res.status = 200;
+	res.send({message: 'test'});
+});
+
 server.listen(server_port, server_ip_address, function () {
   console.log( "Listening on " + server_ip_address + ", port " + server_port )
 });
