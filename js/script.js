472a1c395e228ed4c0c35fe494992a9b4e68f2a4
albeechen <linda3632000@hotmail.com>
2017-09-01 23:13:54 -0700

    create index.html

---
+++ js/script.js
@@ -0,0 +1,22 @@
+
+
+
+
+
+
+
+$(document).ready(function(){
+    $("#hamburger").click(function(){
+    	let menu = document.getElementById('menu');
+	    if (menu.style.display === 'none') {
+	        menu.style.display = 'block';
+	    }
+    });
+
+    $("#clods_btn").click(function(){
+    	let menu = document.getElementById('menu');
+	    if (menu.style.display === 'block') {
+	        menu.style.display = 'none';
+	    } 
+    });
+});
