50fed87e956b0ab22b463fef60ef6b835130cc20
albeechen <linda3632000@hotmail.com>
2017-09-06 14:10:24 -0700

    manager css file and create html file

--- js/script.js
+++ js/script.js
@@ -1,26 +1,30 @@
 $(document).ready(function() {
-    $("#hamburger").click(function() {
-        let menu = document.getElementById('menu');
+   $("#hamburger").click(function() {
+       	let menu = document.getElementById('menu');
+        let sites = document.getElementById('link_sites');
         if (menu.style.display === 'none') {
             menu.style.display = 'block';
+        }else{
+            menu.style.display = 'none';
+            sites.style.display = 'none';
         }
     });
 
-    $("#clods_btn").click(function() {
+   /* $("#clods-btn").click(function() {
         let menu = document.getElementById('menu');
         let sites = document.getElementById('link_sites');
         if (menu.style.display === 'block') {
             menu.style.display = 'none';
             sites.style.display = 'none';
         }
-    });
+    });*/
 
-    $("#link_btn").click(function() {
+    $("#link-btn").click(function() {
         let sites = document.getElementById('link_sites');
         if (sites.style.display === 'none') {
             sites.style.display = 'block';
         } else {
         	sites.style.display = 'none';
         }
-    });
+    });   
 });
